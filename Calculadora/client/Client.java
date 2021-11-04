d08e340c873192ddf895dc192e67fe05b8cc4789
Alan Diaz <calix35@gmail.com>
2021-11-03 18:27:10 -0600

    Calculadora con RMI

--- Calculadora/client/Client.java
+++ Calculadora/client/Client.java
@@ -3,6 +3,8 @@ package client;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 
+import java.util.Scanner;
+
 import common.Calculadora;
 
 public class Client{
@@ -18,10 +20,58 @@ public class Client{
             //Obtengo el objeto remoto
             Calculadora calculadora = (Calculadora)registry.lookup("Calculadora");
 
-            //Ejecuto el metodo remoto
-            float resp = calculadora.suma(2, 3);
-            //Imprimo la respuesta
-            System.out.println(resp);
+            Scanner sc = new Scanner(System.in);
+            int opcion;
+            float a,b, resp;
+            do{
+                System.out.println("1) Suma");
+                System.out.println("2) Resta");
+                System.out.println("3) Multiplicacion");
+                System.out.println("4) Division");
+                System.out.println("5) Salir");
+                System.out.print("Opcion: ");
+                opcion = sc.nextInt();
+                switch(opcion) {
+                    case 1: 
+                        System.out.print("a = ");
+                        a = sc.nextFloat();
+                        System.out.print("b = ");
+                        b = sc.nextFloat();
+                        resp = calculadora.suma(a, b);
+                        System.out.println(resp);
+                        break;
+                    case 2: 
+                        System.out.print("a = ");
+                        a = sc.nextFloat();
+                        System.out.print("b = ");
+                        b = sc.nextFloat();
+                        resp = calculadora.resta(a, b);
+                        System.out.println(resp);
+                        break;
+                    case 3: 
+                        System.out.print("a = ");
+                        a = sc.nextFloat();
+                        System.out.print("b = ");
+                        b = sc.nextFloat();
+                        resp = calculadora.multiplicacion(a, b);
+                        System.out.println(resp);
+                        break;
+                    case 4: 
+                        System.out.print("a = ");
+                        a = sc.nextFloat();
+                        System.out.print("b = ");
+                        b = sc.nextFloat();
+                        resp = calculadora.division(a, b);
+                        System.out.println(resp);
+                        break;
+                    case 5: 
+                        break;
+                    default: 
+                        System.out.println("Operacion no permitida!");
+                }
+            }while(opcion!=5);
+            
+            
         } catch (Exception e) {
             //TODO: handle exception
         }
